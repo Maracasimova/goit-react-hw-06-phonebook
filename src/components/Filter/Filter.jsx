862bbbf3a88bfe80db45e995d5f33579607cfcb5
Casimova Marina <maracasimova@gmail.com>
2023-06-22 23:28:39 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -19,6 +19,7 @@ export const Filter = () => {
         name="filter"
         type="text"
         value={filter}
+        placeholder="search..."
         onChange={handleChangeFilter}
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
