7671c53fd2ddda301682f9912a72930e2ed121ca
Casimova Marina <maracasimova@gmail.com>
2023-06-22 20:38:18 +0300

    Update ContactUser.jsx

--- src/components/ContactUser/ContactUser.jsx
+++ src/components/ContactUser/ContactUser.jsx
@@ -1,15 +1,19 @@
+import { useDispatch } from 'react-redux';
+import { deleteContact } from '../../redux/contactSlice';
 import style from './ContactUser.module.css';
 import PropTypes from 'prop-types';
 
-export const ContactUser = ({ id, name, number, deleteContact }) => {
+export const ContactUser = ({ id, name, number }) => {
+  const dispatch = useDispatch();
+
+  const handleDeleteContact = () => {
+    dispatch(deleteContact(id));
+  };
+
   return (
     <li>
       {name} : {number}
-      <button
-        className={style.btn}
-        type="button"
-        onClick={() => deleteContact(id)}
-      >
+      <button className={style.btn} type="button" onClick={handleDeleteContact}>
         Delete
       </button>
     </li>
