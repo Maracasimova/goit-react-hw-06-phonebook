6869500ffb646ea23601e3c1f65aec0f063dd652
Casimova Marina <maracasimova@gmail.com>
2023-06-22 20:31:28 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,20 +1,32 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteContact } from '../../redux/contactSlice';
 import { ContactUser } from 'components/ContactUser/ContactUser';
 import PropTypes from 'prop-types';
 
-export const ContactList = ({ contactSearch, deleteContact }) => {
+export const ContactList = () => {
+  const contacts = useSelector(state => state.contacts);
+  const filter = useSelector(state => state.filter);
+  const dispatch = useDispatch();
+
+  const handleDeleteContact = id => {
+    dispatch(deleteContact(id));
+  };
+
+  const contactSearch = contacts.filter(({ name }) => {
+    return name.toLowerCase().includes(filter.toLowerCase());
+  });
+
   return (
     <ul>
-      {contactSearch.map(({ name, number, id }) => {
-        return (
-          <ContactUser
-            id={id}
-            key={id}
-            name={name}
-            number={number}
-            deleteContact={deleteContact}
-          />
-        );
-      })}
+      {contactSearch.map(({ id, name, number }) => (
+        <ContactUser
+          id={id}
+          key={id}
+          name={name}
+          number={number}
+          deleteContact={handleDeleteContact}
+        />
+      ))}
     </ul>
   );
 };
